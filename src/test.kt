02940973251d564499f467f63b498e0a50b74af3
dudals3844 <44514383+dudals3844@users.noreply.github.com>
2020-09-06 15:04:33 +0900

    문자열 템플릿 사용

--- src/test.kt
+++ src/test.kt
@@ -1,18 +1,14 @@
 import kotlin.math.max
 
 
-fun getStringLength(obj: Any): Int?{
-    if(obj is String){
-        return obj.length
-    }
 
-    return null
-}
 
 fun main(){
-    var a = "Strings"
-
-    var leng = getStringLength(obj = a)
-    println(leng)
+    var a = 1
+    var s1 = "a is $a"
+    println(s1)
+    a = 2
+    val s2 = "${s1.replace("is", "was")}, but now is $a"
+    print(s2)
 }
 
