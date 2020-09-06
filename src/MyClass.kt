dd6e4df78ee74c8992a28c7c477e8ba95fa4d1a9
dudals3844 <44514383+dudals3844@users.noreply.github.com>
2020-09-06 19:24:14 +0900

    사용자 정의 프로퍼티

--- src/MyClass.kt
+++ src/MyClass.kt
@@ -5,17 +5,28 @@
 //
 // 주요생성자는 constructor 생략 가능
 
-class User {
-    var name :String = "choi"
-    val age :Int = 25
+class User1 {
+    var greeting :String = "Hello"
+        set(value) {
+            field = "Hello" + value
+        }
+        get() = field.toUpperCase()//대문자로 바꿔준다
+
+    var age: Int = 0
+        set(value) {
+            if (value > 0){
+                field = value
+            } else {
+                field = 0
+            }
+        }
 
 }
 
 fun main(args: Array<String>){
-    val user = User()
-
-    user.name = "choi"
-
-    println("${user.name}")
-    println("${user.age}")
+    val user1 = User1()
+    user1.greeting = "choi"
+    println(user1.greeting)
+    user1.age = -24
+    println("${user1.age}")
 }
