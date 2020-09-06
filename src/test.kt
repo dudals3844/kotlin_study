cf56b2ebba3ef568fd5baa90703e3f2e8596a500
dudals3844 <44514383+dudals3844@users.noreply.github.com>
2020-09-06 15:21:36 +0900

    while문 사용

--- src/test.kt
+++ src/test.kt
@@ -1,19 +1,14 @@
 import kotlin.math.max
 
 
-fun getString(obj: Any?): Int?{//?는 Null값이 올수도 있다는 말이다
-    if(obj is String){
-        return obj.length
-    }
-
-    return null
-}
 
 
 fun main(){
     var items = listOf("apple", "banana","kiwi")
-    for (item in items){
-        println(item)
+    var index = 0
+    while (index < items.size){
+        println("item at $index, is ${items[index]}")
+        index++
     }
 }
 
