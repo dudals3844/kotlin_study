4c765704dd8ddfd7a203c32ec2a15d43d6a3dbc8
dudals3844 <44514383+dudals3844@users.noreply.github.com>
2020-09-06 15:19:28 +0900

    for문 사용

--- src/test.kt
+++ src/test.kt
@@ -1,14 +1,19 @@
 import kotlin.math.max
 
 
+fun getString(obj: Any?): Int?{//?는 Null값이 올수도 있다는 말이다
+    if(obj is String){
+        return obj.length
+    }
+
+    return null
+}
 
 
 fun main(){
-    var a = 1
-    var s1 = "a is $a"
-    println(s1)
-    a = 2
-    val s2 = "${s1.replace("is", "was")}, but now is $a"
-    print(s2)
+    var items = listOf("apple", "banana","kiwi")
+    for (item in items){
+        println(item)
+    }
 }
 
