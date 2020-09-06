98b1bb0fcb2b4f4957483cc081714d4274f4bec7
dudals3844 <44514383+dudals3844@users.noreply.github.com>
2020-09-06 15:28:55 +0900

    범위를 반복

--- src/test.kt
+++ src/test.kt
@@ -4,9 +4,8 @@ import kotlin.math.max
 
 
 fun main(){
-    val list = listOf("a", "b", "c")
-    if (-1 !in 0..list.lastIndex){
-        println("-1 is out of range")
+    for (x in 1..5){
+        println(x)
     }
 }
 
