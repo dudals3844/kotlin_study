b45c4b822e1525c4093f320901774c36be7ce180
dudals3844 <44514383+dudals3844@users.noreply.github.com>
2020-09-06 18:22:02 +0900

    생성자 오버라이딩

--- src/MyClass.kt
+++ src/MyClass.kt
@@ -4,23 +4,15 @@
 //}
 //
 // 주요생성자는 constructor 생략 가능
-class Person(firstName: String){
 
-}
-// 기본 생성자
-class User1{
-
-}
-// 주생성자
-class User2(name: String){
+class User4{
+    constructor(){}
+    constructor(name: String){}
+    constructor(name: String, age: Int){}
 
 }
 
-//보조 생성자
-class User3{
-    constructor(name: String)
-}
 fun main(args: Array<String>){
-  val user4 = User4(name = "choi", age = 24)
-    user4.sayHello()
+    val user4 = User4()
+    val user5 = User4("choi", 35)
 }
