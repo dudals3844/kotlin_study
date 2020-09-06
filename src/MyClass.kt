6f6c71ed7d4b6e4808ea9bad8949de384e04f46a
dudals3844 <44514383+dudals3844@users.noreply.github.com>
2020-09-06 18:16:31 +0900

    생성자

--- src/MyClass.kt
+++ src/MyClass.kt
@@ -1,18 +1,27 @@
-
-
-class MyClass{
-    var name: String = "world"
-
-    fun sayHello(){
-        println("hello $name")
-
-    }
+//주요 생성자
+//class Person constructor(firstName: String){
+//
+//}
+//
+// 주요생성자는 constructor 생략 가능
+class Person(firstName: String){
 
 }
 
+class User4(name: String, age: Int){
+    val myName = name
+    // 생성자 초기화 블록
+    init {
+        println("I am init")
 
+        println("$name,     $age")
+    }
+
+    fun sayHello(){
+        println("hello $myName")
+    }
+}
 fun main(args: Array<String>){
-    val obj1 = MyClass()
-    val obj2 = MyClass()
-    obj1.sayHello()
+  val user4 = User4(name = "choi", age = 24)
+    user4.sayHello()
 }
