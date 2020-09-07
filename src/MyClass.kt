656abeaa5e4534fdebfd69c17b14ca8abb5f42a9
YoungMin <dudals3844@gmail.com>
2020-09-07 18:02:24 +0900

    객체 타입으로서의 인터페이스

--- src/MyClass.kt
+++ src/MyClass.kt
@@ -1,28 +1,35 @@
 import kotlin.properties.Delegates
 
 
-interface MyInterface6{
-    fun myFunc4()
+interface MyInterface10{
+    fun myInterfaceFun()
 }
 
-interface MyInterface7{
-    fun myFunc5()
-}
-
-open class Super{
-
-}
-class Sub: Super(), MyInterface6, MyInterface7{
-    override fun myFunc4() {
-        TODO("Not yet implemented")
-    }
-
-    override fun myFunc5() {
-        TODO("Not yet implemented")
+open class Super1{
+    fun mySuperFunc(){
+        println("mySuperFun")
     }
 }
+
+class Sub1: Super1(), MyInterface10{
+    override fun myInterfaceFun() {
+        println("myInterfaceFun cal.....")
+    }
+}
+
 
 fun main(args: Array<String>){
+    val obj1: Sub1 = Sub1()
+    val obj2: Super1 = Sub1()
+    val obj3: MyInterface10 = Sub1()
 
+    obj1.mySuperFunc()
+    obj1.myInterfaceFun()
+
+    obj2.mySuperFunc()
+//    obj2.myInterfaceFun() //error
+
+//    obj3.mySuperFunc() //error
+    obj3.myInterfaceFun()
 
 }
