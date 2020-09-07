49a0d3fb4bb958d6760c5639a623273b89a14575
dudals3844 <44514383+dudals3844@users.noreply.github.com>
2020-09-07 14:04:43 +0900

    추상 클래스

--- src/MyClass.kt
+++ src/MyClass.kt
@@ -1,24 +1,22 @@
 import kotlin.properties.Delegates
 
 
-val myData1 :Int = 10
-private val myData2 :String = "hello"
-class Myclass1(){}
-private class MyClass2(){}
-fun myFunc1(){
-    println("myFun() call...")
+abstract class AbstractTest1{
+    val data1 :Int = 10
+    abstract val data2 :Int
+    fun myFun1(){
+
+    }
+    abstract fun myFun2()
 }
 
-private fun myFunc2(){
-    println("myFun()2 call...")
+class Sub: AbstractTest1(){
+    override val data2 :Int = 15
+    override fun myFun2() {
+        TODO("Not yet implemented")
+    }
 }
-
 
 fun main(args: Array<String>){
-    println("$myData1")
-    println("$myData2")
-    val obj1 = Myclass1()
-    val obj2 = MyClass2()
-    myFunc1()
-    myFunc2()
+    val obj1 = Sub()
 }
