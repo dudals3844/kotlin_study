0f1f7da53ab4d696eeab4d9796cf87f099785a20
dudals3844 <44514383+dudals3844@users.noreply.github.com>
2020-09-07 14:00:48 +0900

    접근 제한자

--- src/MyClass.kt
+++ src/MyClass.kt
@@ -1,26 +1,24 @@
 import kotlin.properties.Delegates
 
-open class Super{
-    constructor(name :String, no :Int){
-        println("Super... constructor(name $name, no $no)")
-    }
-    init {
-        println("Super .... init call")
-    }
+
+val myData1 :Int = 10
+private val myData2 :String = "hello"
+class Myclass1(){}
+private class MyClass2(){}
+fun myFunc1(){
+    println("myFun() call...")
 }
 
-class Sub(name :String): Super(name, 10){
-    constructor(name: String, no :Int): this(name){
-        println("Sub... constructor(name $name, no $no) call")
-    }
-    init {
-        println("Sub ... init call")
-    }
+private fun myFunc2(){
+    println("myFun()2 call...")
 }
+
 
 fun main(args: Array<String>){
-    Sub("choi")
-    println("--------")
-    Sub("min", 24)
-
+    println("$myData1")
+    println("$myData2")
+    val obj1 = Myclass1()
+    val obj2 = MyClass2()
+    myFunc1()
+    myFunc2()
 }
