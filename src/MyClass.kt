8cfb2a765c57ff3422b55f6dd4e9c81180c69cba
dudals3844 <44514383+dudals3844@users.noreply.github.com>
2020-09-07 11:07:41 +0900

    프로퍼티 변경감지

--- src/MyClass.kt
+++ src/MyClass.kt
@@ -1,3 +1,5 @@
+import kotlin.properties.Delegates
+
 //주요 생성자
 //class Person constructor(firstName: String){
 //
@@ -5,32 +7,17 @@
 //
 // 주요생성자는 constructor 생략 가능
 
-val someData: String by lazy {
-    println("I am someData lazy....")
-    "hello"
+class User {
+    var name: String by Delegates.observable("nonValue", {props, old, new ->
+        println("old $old ..... new $new")
+    })
 }
 
-class User1{
-    val name: String by lazy {
-        println("I am name lazy")
-        "choi"
-    }
 
-    val age: Int by lazy {
-        println("I am age lazy")
-        24
-    }
-
-    init {
-        println("I am init")
-    }
-    constructor(){
-        println("I am constructor")
-
-    }
-}
 fun main(args: Array<String>){
-    val user1 = User1()
-    println(user1.name)
-    println(user1.age)
+    val user = User()
+    println(user.name)
+    user.name = "choi"
+    user.name = "young"
+    println(user.name)
 }
