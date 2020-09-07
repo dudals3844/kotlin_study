2ec7a695d7f0daa8d8db926edc97caa159ac0a89
dudals3844 <44514383+dudals3844@users.noreply.github.com>
2020-09-07 13:46:51 +0900

    오버라이드

--- src/MyClass.kt
+++ src/MyClass.kt
@@ -1,54 +1,23 @@
 import kotlin.properties.Delegates
 
-open class Shape {
-    var x :Int = 0
-        set(value) {
-            if(value < 0) field = 0
-            else field = value
-        }
-    var y :Int = 0
-        set(value) {
-            if (value < 0) field = 0
-            else field = value
-        }
-
-    lateinit var name :String
-
-    fun print(){
-        println("$name : location: $x $y")
+open class Super {
+    open var x :Int = 10
+    open fun someFun(){
+        println("Super...someFunc()")
     }
 }
 
-class Rect: Shape(){
-    var width :Int = 0
-        set(value) {
-            if (value < 0) field = 0
-            else field = value
-        }
 
-    var height :Int = 0
-        set(value) {
-            if (value < 0) field = 0
-            else field = value
-        }
+class Sub :Super(){
+    override var x :Int = 20
+    override fun someFun() {
+        super.someFun()
+        println("Sub....${super.x}.....$x")
+    }
 }
-
-class Circle: Shape(){
-    var r :Int = 0
-        set(value) {
-            if (value < 0) field = 0
-            else field = value
-        }
-}
-
-
 
 
 fun main(args: Array<String>){
-    val obj1: Any = Shape()
-    val obj2: Any = Shape()
-
-    val obj3 = obj1
-    println("${obj1.equals(obj2)}")
-    println("${obj1.equals(obj3)}")
+    var sub = Sub()
+    sub.someFun()
 }
