ecc8d1e33425b7df3bee6cf9d5d2cf37318bd4c2
YoungMin <dudals3844@gmail.com>
2020-09-09 09:27:26 +0900

    data class 데이터를 문자열로 변환

--- src/MyClass.kt
+++ src/MyClass.kt
@@ -1,18 +1,21 @@
+import java.nio.file.attribute.UserDefinedFileAttributeView
 import kotlin.properties.Delegates
 
-class Product(val name :String, val price :Int)
+class Product(val name:String, val price:Int)
 
-data class User(val name:String, val age :Int)
+data class User(val name:String, val age :Int){
+    var email :String = "dudals@gmail.com"
+}
 
 
 
 fun main(args: Array<String>){
+
     var product = Product("prod1", 100)
-    var product1 = Product("prod1", 100)
-    println(product.equals(product1))
+    println(product.toString())
 
-    var user = User("choi", 30)
-    var user1 = User("choi", 30)
+    val user2 = User("young", 24)
+    user2.email = "choi@hanmial.com"
 
-    println(user.equals(user1))
+    println(user2.toString())
 }
