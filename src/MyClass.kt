e43536091dca743a550290f29747114a9b347e02
YoungMin <dudals3844@gmail.com>
2020-09-09 11:01:15 +0900

    고차함수 함수 참조를 이용한 함수 전달

--- src/MyClass.kt
+++ src/MyClass.kt
@@ -5,13 +5,33 @@ import kotlin.properties.Delegates
 
 
 
+class User(){
+    var name: String? = null
+    var age: Int? = null
+
+    fun sayHello(){
+        println("hello $name")
+    }
+    fun sayInfo(){
+        println("I am $name, $age years old")
+    }
+}
+
 fun main(args: Array<String>){
-    val anomyFun = fun(x: Int): Int = x *10
-    val anomyFun2 = fun(x: Int): Int{
-        println("I am anonymous function")
-        return x * 10
+    var user = User()
+    user.name = "choi"
+    user.age = 25
+    user.sayHello()
+    user.sayInfo()
+
+
+    val runResult = user.run {
+        name = "young"
+        age = 24
+        sayHello()
+        sayInfo()
+        10 + 20
     }
 
-    println(anomyFun(5))
-    println(anomyFun2(10))
+    println("run result: $runResult")
 }
