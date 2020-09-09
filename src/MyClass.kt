1a41f11ac08686b972ef0f9940faa6e12da6fd34
YoungMin <dudals3844@gmail.com>
2020-09-09 13:19:24 +0900

    예외처리와 사용자 정의 예외

--- src/MyClass.kt
+++ src/MyClass.kt
@@ -1,21 +1,28 @@
+import java.lang.Exception
 import java.lang.constant.DirectMethodHandleDesc
 import java.net.spi.URLStreamHandlerProvider
 import java.nio.file.attribute.UserDefinedFileAttributeView
 import kotlin.properties.Delegates
 
-//null 안정성
+
+
+class MyException(msg: String):Exception(msg){
+    val errorData: String = "some error data"
+    fun errorFun(){
+        println("errorFun call.....")
+    }
+}
+
+fun some1(){
+    throw MyException("My error....")
+}
 
 fun main(args: Array<String>){
-    val array = arrayOf("hello", null, "kkang")
-    array.forEach {
-        if (it != null){
-            print(" $it ..... ${it.length}")
-        }
-    }
-
-    array.forEach {
-        it?.let {
-            println("$it......${it.length}")
-        }
+    try {
+        some1()
+    } catch (e: MyException){
+        println("error message: ${e.toString()}")
+        println("error data: ${e.errorData}")
+        e.errorFun()
     }
 }
