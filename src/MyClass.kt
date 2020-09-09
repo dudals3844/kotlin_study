1aeab5877a376a721e584df01aaf98dad4d47f4b
YoungMin <dudals3844@gmail.com>
2020-09-09 09:48:03 +0900

    enum class 임의 데이터 삽입

--- src/MyClass.kt
+++ src/MyClass.kt
@@ -2,12 +2,16 @@ import java.nio.file.attribute.UserDefinedFileAttributeView
 import kotlin.properties.Delegates
 
 
-enum class Direction(val no: Int){
-    NORTH(1), SOUTH(2), WEST(3), EAST(4)
+enum class Direction(var no: Int, val str: String){
+    NORTH(1,"north"), SOUTH(2,"south"), WEST(3,"west"), EAST(4,"east")
 }
 
 
 fun main(args: Array<String>){
     val direction: Direction = Direction.NORTH
-    println(Direction.NORTH.no)
+    println("${direction.no}....${direction.str}")
+
+    direction.no = 10
+    println("${direction.no}....${direction.str}")
+
 }
