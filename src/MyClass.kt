a4ac688a5928f9a29d3e72254ce27ceaa0d2c334
YoungMin <dudals3844@gmail.com>
2020-09-09 09:33:12 +0900

    data class 클래스 프로퍼티값 획득

--- src/MyClass.kt
+++ src/MyClass.kt
@@ -1,21 +1,18 @@
 import java.nio.file.attribute.UserDefinedFileAttributeView
 import kotlin.properties.Delegates
 
-class Product(val name:String, val price:Int)
 
-data class User(val name:String, val age :Int){
-    var email :String = "dudals@gmail.com"
-}
+data class User(val name:String, val age :Int)
 
 
 
 fun main(args: Array<String>){
+    var user = User("choi", 25)
 
-    var product = Product("prod1", 100)
-    println(product.toString())
+    println(user.component1())
+    println(user.component2())
 
-    val user2 = User("young", 24)
-    user2.email = "choi@hanmial.com"
 
-    println(user2.toString())
+    val (name, age) = user
+    println("name: $name, age: $age")
 }
