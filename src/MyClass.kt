022bf6b3301397ee022e96ad988276a409d93a66
YoungMin <dudals3844@gmail.com>
2020-09-10 09:37:10 +0900

    제네릭 여러개의 타입으로 제약

--- src/MyClass.kt
+++ src/MyClass.kt
@@ -7,20 +7,17 @@ import kotlin.properties.Delegates
 // 제네릭
 //타입을 예측할 수 없는 경우
 
-//class MyClass{
-//    var info: String? = null
-//}
+interface MyInterface1
+interface MyInterface2
 
+class MyHandler2: MyInterface1, MyInterface2
+class MyHandler1: MyInterface1
 
-class MathUtil<T: Number>{
-    fun plus(arg1: T, arg2: T): Double{
-        return arg1.toDouble() + arg2.toDouble()
-    }
+class MyClass<T> where T: MyInterface2, T:MyInterface1{//13번줄 떄문에 2개의 타입으로 와야된다.
+
 }
 
 fun main(args: Array<String>){
-    val obj = MathUtil<Int>()
-    var add = obj.plus(10, 20)
-    println(add)
-    val obj2 = MathUtil<Double>()
+//    val obj = MyClass<MyHandler1>()
+    val obj2 =MyClass<MyHandler2>()
 }
