9b9fd2339cdd2688022d0467b21f3d9913684c5f
YoungMin <dudals3844@gmail.com>
2020-09-10 09:40:16 +0900

    제네릭 null 불허 제약

--- src/MyClass.kt
+++ src/MyClass.kt
@@ -7,17 +7,16 @@ import kotlin.properties.Delegates
 // 제네릭
 //타입을 예측할 수 없는 경우
 
-interface MyInterface1
-interface MyInterface2
-
-class MyHandler2: MyInterface1, MyInterface2
-class MyHandler1: MyInterface1
-
-class MyClass<T> where T: MyInterface2, T:MyInterface1{//13번줄 떄문에 2개의 타입으로 와야된다.
-
+class MyClass<T>{
+    fun myFun(arg1: T, arg2: T){
+        println(arg1?.equals(arg2))
+    }
 }
 
 fun main(args: Array<String>){
-//    val obj = MyClass<MyHandler1>()
-    val obj2 =MyClass<MyHandler2>()
+    val obj = MyClass<String>()
+    obj.myFun("hello", "hello")
+    val obj2 = MyClass<Int?>()
+    obj2.myFun(null, 10)
+
 }
