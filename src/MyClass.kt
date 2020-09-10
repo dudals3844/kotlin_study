323b16199225fcb7bb3045d059ca231d6529ddd2
YoungMin <dudals3844@gmail.com>
2020-09-10 10:17:56 +0900

    제네릭 타입 제약 확인

--- src/MyClass.kt
+++ src/MyClass.kt
@@ -2,28 +2,24 @@ import java.lang.Exception
 import java.lang.constant.DirectMethodHandleDesc
 import java.net.spi.URLStreamHandlerProvider
 import java.nio.file.attribute.UserDefinedFileAttributeView
+import javax.print.attribute.SupportedValuesAttribute
+import javax.swing.plaf.BorderUIResource
 import kotlin.properties.Delegates
 
 // 제네릭
 //타입을 예측할 수 없는 경우
 
+//타입제한 복습
 
-open class Super{
-    open fun sayHello(){
-        println("I am sayHello")
-    }
-}
-
-class Sub: Super(){
-    override fun sayHello() {
-        println("I am sub sayHello...")
+class MathUtil<T: Number>{//숫자에 관련된 타입으로 제약한다
+    fun plus(arg1: T, arg2: T): Double{
+        return arg1.toDouble() + arg2.toDouble()
     }
 }
 
 fun main(args: Array<String>){
-    val obj: Super = Sub()
-    obj.sayHello()
+    val obj = MathUtil<Int>()
+    obj.plus(10, 20)
 
-    val obj2: Sub = obj as Sub
-    obj2.sayHello()
+    val obj2 = MathUtil<Double>()
 }
