0e0bd3feb8e23689399023cdfcad9118cf12fdf0
YoungMin <dudals3844@gmail.com>
2020-09-14 16:29:53 +0900

    리플렉션 사용

--- src/MyClass.kt
+++ src/MyClass.kt
@@ -5,42 +5,32 @@ import java.nio.file.attribute.UserDefinedFileAttributeView
 import javax.print.attribute.SupportedValuesAttribute
 import javax.swing.plaf.BorderUIResource
 import kotlin.properties.Delegates
-
-// 제네릭
-//타입을 예측할 수 없는 경우
-
-//타입제한 복습
-
-class MyClass<T>(val data: T){
-    fun myFun(): T{
-        return data
-    }
-    fun myFun2(arg: T){
-
-    }
-    fun myFun3(arg: T): T{
-        return data
-    }
-}
-
-fun some2(arg: MyClass<out Number>){
-    arg.myFun()
-//    arg.myFun2() 타입을 모른다고 에러남
-//    arg.myFun3(10)
-}
-
-class Test<T>(val num: T){
-    fun test(arg: T): T{
-        return arg
-    }
-}
+import kotlin.reflect.KClass
+import kotlin.reflect.KMutableProperty
+import kotlin.reflect.KProperty
+//
+////어노테이션
+//
+//val myVal: KClass<*> = String::class
+//fun myFun(arg: KClass<*>){
+//
+//}
+//
+//val myVal2: KClass<String> = String::class
+//
+//val myVal3: Class<*> = String::class.java
 
 
+val myVal:Int = 10
+var myVar: Int = 10
+
+val referenceVal1: KProperty<*> = ::myVal
+val referenceVal2: KProperty<*> = ::myVar
+
+//val referenceVal4: KMutableProperty<*> = ::myVar
 fun main(args: Array<String>){
-    some2(MyClass<Number>(10))
-    some2(MyClass<Int>(10))
-
-    var test = Test<Int>(5)
-    println(test.test(4))
-
+    println(::myVal.get())
+    ::myVar.set(40)
+    println(::myVar.get())
+    println(::myVal.isFinal)
 }
