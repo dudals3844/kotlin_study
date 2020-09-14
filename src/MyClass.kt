ad5ac39a4f45be0d6d51eb57ace26df583dad51d
YoungMin <dudals3844@gmail.com>
2020-09-14 17:11:24 +0900

    어노테이션 허용 데이터 타입

--- src/MyClass.kt
+++ src/MyClass.kt
@@ -1,13 +1,5 @@
-import java.lang.Exception
-import java.lang.constant.DirectMethodHandleDesc
-import java.net.spi.URLStreamHandlerProvider
-import java.nio.file.attribute.UserDefinedFileAttributeView
-import javax.print.attribute.SupportedValuesAttribute
-import javax.swing.plaf.BorderUIResource
-import kotlin.properties.Delegates
 import kotlin.reflect.KClass
-import kotlin.reflect.KMutableProperty
-import kotlin.reflect.KProperty
+
 //
 ////어노테이션
 //
@@ -20,26 +12,19 @@ import kotlin.reflect.KProperty
 //
 //val myVal3: Class<*> = String::class.java
 
-annotation class TestAnnotation(val count: Int)
+annotation class TestAnnotation1(val count: Int)
+
+annotation class TestAnnotation2(val otherAnn:TestAnnotation1, val arg1: KClass<*>)
+
+class User
+
+@TestAnnotation1(10)
+@TestAnnotation2(TestAnnotation1(20), String::class)
 
 class Test{
-    @TestAnnotation(count = 3)
-    fun some(){
-        println("Some....")
-    }
+
 }
 
-fun main(args: Array<String>){
-    val obj: Test = Test()
-    val methods = Test::class.java!!.methods
-
-    for (method in methods){
-        if (method.isAnnotationPresent(TestAnnotation::class.java)){
-            val annotation = method.getAnnotation(TestAnnotation::class.java)
-            val count = annotation.count
-            for (i in 1..count){
-                obj.some()
-            }
-        }
-    }
-}
+const val myData: Int = 10
+@TestAnnotation1(myData)
+class Test2{}
