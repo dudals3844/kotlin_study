4da25ca3230ba553c9874783d77ad52619e7f44a
YoungMin <dudals3844@gmail.com>
2020-09-14 17:55:19 +0900

    인터페이스 선언 및 구현

--- src/MyClass.kt
+++ src/MyClass.kt
@@ -5,26 +5,25 @@ import kotlin.system.exitProcess
 ////어노테이션
 //
 
-abstract class Super{
-    val data1: Int = 10
-    abstract val data2: Int
-
+interface MyInterface{
+    var data1: String
     fun myFun1(){
 
     }
+    fun myFun2(){
 
-    abstract fun myFun2()
-
+    }
 }
 
-class Sub: Super(){
-    override val data2: Int = 10
+class MyClass: MyInterface{
+    override var data1: String = "choi"
     override fun myFun2() {
 
     }
 }
 
-
 fun main(args: Array<String>){
-    val obj1 = Sub()
+    val obj1 = MyClass()
+    obj1.myFun1()
+    obj1.myFun2()
 }
