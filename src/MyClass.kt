295b73b1bcc87e00a8102cbea5da3cde2257fe86
YoungMin <dudals3844@gmail.com>
2020-09-14 18:18:52 +0900

    클래스에서 여러 인터페이스 구현

--- src/MyClass.kt
+++ src/MyClass.kt
@@ -4,26 +4,24 @@ import kotlin.system.exitProcess
 //
 ////어노테이션
 //
+interface MyInterface4{
+    fun myFun4()
 
-interface MyInterface{
-    var data1: String
-    fun myFun1(){
-
-    }
-    fun myFun2(){
-
-    }
 }
 
-class MyClass: MyInterface{
-    override var data1: String = "choi"
-    override fun myFun2() {
+interface MyInterface5{
+    fun myFun5()
+}
+
+class MyClass4: MyInterface4, MyInterface5{
+    override fun myFun4() {
+    }
+
+    override fun myFun5() {
 
     }
 }
 
 fun main(args: Array<String>){
-    val obj1 = MyClass()
-    obj1.myFun1()
-    obj1.myFun2()
+
 }
