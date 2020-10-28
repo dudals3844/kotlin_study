194cbac598129c9ed50c4b144c0492c70c02079f
YoungMin <dudals3844@gmail.com>
2020-10-28 11:38:08 +0900

    간단한 클래스

--- src/MyClass.kt
+++ src/MyClass.kt
@@ -3,30 +3,19 @@ import java.io.*
 import java.net.Socket
 import kotlin.math.atan2
 
+class MyClass {
+    var name: String = "world"
+    fun sayHello(){
+        println("hell $name")
+    }
+}
+
+
 fun main(args: Array<String>){
-    val a = 5
-    if(a < 10){
-        println("$a < 10")
-    } else if (a > 10 && a <= 20){
-        println("10 < $a <= 20")
-    }
-
-
-    //switch랑 비슷
-    when(a){
-        5 -> println("Hello")
-    }
-
-    for(i in 1..10){
-        println(i)
-    }
-
-    var list = listOf("Hello", "World", "!!")
-    val sb = StringBuffer()
-    for (str in list){
-        sb.append(str)
-    }
-    print(sb)
+    val obj1 = MyClass()
+    val obj2 = MyClass()
+    obj1.name = "choi"
+    obj1.sayHello()
 }
 
 
